508dfbf8f8a31a35037b09b1e55c3a55bab48e56
Cody Wheeler <codywh2@gmail.com>
2024-06-25 03:56:02 -0400

    added looping music

--- Source/LawnMower/MowerGameModeA.cpp
+++ Source/LawnMower/MowerGameModeA.cpp
@@ -1,6 +1,29 @@
 // Derived AGameMode class AMowerGameModeA by Cody Wheeler.
 
 
-#include "MowerGameModeA.h"
+#include"MowerGameModeA.h"
+
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
+AMowerGameModeA::AMowerGameModeA()
+{
+	SongLoop = CreateDefaultSubobject<UAudioComponent>(TEXT("SongLoop"));
+
+	SongLoop->SetupAttachment(RootComponent);
+}
+
+
+void AMowerGameModeA::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetActorTickEnabled(true);
+}
+
+
+void AMowerGameModeA::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
--- Source/LawnMower/MowerGameModeA.h
+++ Source/LawnMower/MowerGameModeA.h
@@ -7,10 +7,25 @@
 #include "GameFramework/GameMode.h"
 #include "MowerGameModeA.generated.h"
 
+class UAudioComponent;
+
 
 UCLASS()
 class LAWNMOWER_API AMowerGameModeA : public AGameMode
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(EditDefaultsOnly, Category = Component) UAudioComponent* SongLoop {};
+
+public:
+	AMowerGameModeA();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
 };
